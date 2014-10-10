7b94550fe600f33650918c526e9671b6e5735785
Victor Gut <gtrrz.victor@gmail.com>
2014-10-10 11:44:11 +0200

    multi-proyect has been created

---
+++ dao/src/main/java/gtrrzvictor/neo4j/concept/dao/App.java
@@ -0,0 +1,13 @@
+package gtrrzvictor.neo4j.concept.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
